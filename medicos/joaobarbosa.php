72faeee36012ec68bee6933e98dea4f027fa17d4
an1car <109595625+an1car@users.noreply.github.com>
2022-09-05 18:37:54 -0300

    Update joaobarbosa.php

--- medicos/joaobarbosa.php
+++ medicos/joaobarbosa.php
@@ -32,7 +32,7 @@ foreach( $get_consulta as $row){
     <link rel="stylesheet" href="../css/mycss.css" media="screen">
     <link rel="stylesheet" href="../css/joaobarbosa.css" media="screen">
     <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
-    <script class="u-script" type="text/javascript" src="JS/popup.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="popup.js" defer=""></script>
 
     <meta name="theme-color" content="#478ac9">
     <meta property="og:title" content="Contato">
@@ -63,6 +63,7 @@ foreach( $get_consulta as $row){
     <div class="popup">
       <div class="popup-close">X</div>
       <div class="popup-content"></div>
+      <h2>HORÁRIOS DISPONÍVEIS</h2>
               <!--HORÁRIO DISPONÍVEIS NO BANCO DE DADOS  -->
         <center><buttonA data-page-id="821229681" class="u-border-none u-btn u-btn-round u-button-style u-custom-color-1 u-hover-palette-1-light-1 u-radius-20 u-btn-1">Marcar Consulta</buttonA>
     </div>
